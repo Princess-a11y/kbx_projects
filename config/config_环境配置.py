7811a7cd0a585e1885c6fb3483cf2ec8f8054532
Princess-a11y <lr1695813894@163.com>
2026-04-09 01:10:02 +0800

    Add files via upload

---
+++ config/config_环境配置.py
@@ -0,0 +1,11 @@
+# 必须包含以下变量
+BASE_URL = "https://test.kukeda.com:8443/api"
+excel_file = "data/快捷豹用例-接口自动化.xlsx"
+sheet_name = "Sheet1"
+
+# 数据库配置 (如果用到 sql_asserts)
+mysql_host = ""
+mysql_port = 3306
+mysql_user = ""
+mysql_password = ""
+mysql_database = ""
